aea1d9f2ea1a3411499db2402e2926b7eb548f05
Bruno Reis <brunocbreis@gmail.com>
2022-08-04 20:43:59 -0300

    installed locally

---
+++ setup.py
@@ -0,0 +1,13 @@
+import setuptools
+
+setuptools.setup(
+    name="pysion",
+    version="0.1",
+    description="Package for creating code that's readable by DaVinci Resolve Fusion.",
+    url="https://github.com/brunocbreis/pysion",
+    author="Bruno Reis",
+    install_requires=[],
+    author_email="brunocbreis@gmail.com",
+    packages=setuptools.find_packages(),
+    zip_safe=False,
+)
