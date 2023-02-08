b1b7920b46787c07a00b0ddd9987b7c033a58ce9
Bruno Reis <85531625+brunocbreis@users.noreply.github.com>
2023-02-08 11:37:42 -0300

    clean init files

--- pysion/__init__.py
+++ pysion/__init__.py
@@ -1,1 +1,1 @@
-from .generators import add_tool, add_inputs, add_source_input, wrap_for_fusion
+
--- pysion/animation/__init__.py
+++
@@ -1,2 +0,0 @@
-from .generators import add_spline, add_spline_input
-from .curves import Curve, EaseInOut, Linear
