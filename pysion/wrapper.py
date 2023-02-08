9c829786605fde552150216aa8c79a62c0531257
Bruno Reis <85531625+brunocbreis@users.noreply.github.com>
2023-02-08 13:26:02 -0300

    fix: wrapper using join() wrong

--- pysion/wrapper.py
+++ pysion/wrapper.py
@@ -4,7 +4,7 @@ from .tool import Tool
 def wrap_for_fusion(tools: list[Tool]) -> str:
     """Adds header and footer to a sequence of tools"""
 
-    tools_string = [str(tool) for tool in tools].join()
+    tools_string = "".join([str(tool) for tool in tools])
 
     header = "{\n\tTools = ordered() {\n"
     footer = f'\t}},\n\tActiveTool = "{tools[-1].name}"\n}}'
