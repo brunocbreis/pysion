c3c0194e86f334c58233fb626a1a935449573cff
Bruno Reis <85531625+brunocbreis@users.noreply.github.com>
2023-02-08 12:11:03 -0300

    fix: add add_mask to init

--- pysion/__init__.py
+++ pysion/__init__.py
@@ -1,2 +1,8 @@
-from .generators import add_inputs, add_source_input, add_tool, wrap_for_fusion
+from .generators import (
+    add_inputs,
+    add_source_input,
+    add_tool,
+    wrap_for_fusion,
+    add_mask,
+)
 from .utils import fusion_coords, fusion_point, kf_pairs
