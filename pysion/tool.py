0b99c20edcca00d38fd53a7e33fb3098b1fdb6d5
Bruno Reis <85531625+brunocbreis@users.noreply.github.com>
2023-02-13 18:02:34 -0300

    tool uses new generators

--- pysion/tool.py
+++ pysion/tool.py
@@ -1,68 +1,81 @@
 from __future__ import annotations
-from .generators import generate_tool
 from dataclasses import dataclass
-from .input import Input, SourceInput, MaskInput, Polyline, Output
+from .input import Input, Polyline
+from .user_controls import UserControl
 from typing import Literal
-from .utils import RGBA
+from .utils import RGBA, fusion_coords
+from .new_generators import NamedTable, UnnamedTable
 
 
 @dataclass
 class Tool:
+    """Represents a Fusion Tool. Outputs a NamedTable, with the tool ID being the name."""
+
     id: str
     name: str
     position: tuple[int, int] = (0, 0)
-    outputs: list[Output] = None
+    inputs: UnnamedTable[str, Input] | None = None
+    output: str = "Output"
+    user_controls: UnnamedTable[str, UserControl] | None = None
 
-    def __post_init__(self):
-        if not self.outputs:
-            self.outputs = [Output(self.name)]
-
-        self._inputs: dict[str, Input] = {}
-        self._source_inputs: list[SourceInput] = []
-        self._polylines: list[Polyline] = []
-
-    def __str__(self) -> str:
-        inputs = "".join([input.string for input in self.inputs.values()])
-        source_inputs = "".join([input.string for input in self.source_inputs])
-        polylines = "".join([pl.string for pl in self.polylines])
-
-        return generate_tool(
+    # Renderers
+    def render(self) -> NamedTable:
+        return NamedTable(
             self.id,
-            self.name,
-            inputs + source_inputs + polylines,
-            self.position,
+            Inputs=self.inputs,
+            ViewInfo=self.position_nt,
+            UserControls=self.user_controls,
+            force_indent=True,
         )
 
-    @property
-    def string(self) -> str:
-        return self.__str__()
+    def _render_position(self) -> NamedTable:
+        return NamedTable("OperatorInfo", Pos=fusion_coords(self.position))
 
     @property
-    def inputs(self):
-        return self._inputs
+    def position_nt(self) -> NamedTable:
+        return self._render_position()
 
-    def add_inputs(self, type: Literal["Value", "Expression"] = "Value", **kwargs):
-        for k, v in kwargs.items():
-            self._inputs[k] = Input(self.name, k, v, type)
+    def _render_output(self) -> NamedTable:
+        return NamedTable("InstanceOutput", SourceOp=self.name, Source=self.output)
+
+    # Inputs
+    def add_input(self, input: Input) -> Tool:
+        if self.inputs is None:
+            self.inputs = UnnamedTable()
+
+        self.inputs[input.name] = input
 
         return self
 
-    @property
-    def source_inputs(self):
-        return self._source_inputs
+    def add_inputs(self, *inputs, **names_and_values) -> Tool:
+        """Add Inputs as a batch. Takes Input() types as positional args, and/or
+        key/value pairs as keyword args that map to Input(Key, value=Value)"""
 
-    def add_source_input(self, input: str, tool_name: str, tool_output: str = "Output"):
-        self._source_inputs.append(
-            SourceInput(self.name, input, tool_name, tool_output)
-        )
+        if inputs:
+            for input in inputs:
+                self.add_input(input)
+
+        if names_and_values:
+            for k, v in names_and_values.items():
+                self.add_input(Input(k, v))
 
         return self
 
-    def add_color_input(self, color: RGBA, prefix: str = "", suffix: str = ""):
+    def add_published_polyline(self, points: list[tuple[float, float]]) -> Tool:
+        poly = Polyline(points).inputs
+
+        for input in poly:
+            self.inputs[input.name] = input
+
+        return self
+
+    def add_color_input(self, color: RGBA | None, prefix: str = "", suffix: str = ""):
         """Adds color input from RGBA. Different Prefixes and Suffixes are added to color inputs in Fusion.
         Backgrounds usually default to TopLeft[Color] for solid fills.
         Text+ nodes have [Color]1, [Color]2 etc for different layers.
         """
+        if not color:
+            return self
 
         self.add_inputs(
             **{
@@ -75,59 +88,14 @@ class Tool:
 
         return self
 
-    def add_mask(self, mask_name: str):
-        self._source_inputs.append(MaskInput(self.name, mask_name))
+    def add_mask(self, mask: Tool) -> Tool:
+        self.inputs["EffectMask"] = Input(
+            "EffectMask", source_operator=mask.name, source=mask.output
+        )
 
         return self
 
-    def add_published_polyline(
-        self, points: list[tuple[float, float]], point_name: str = "Point"
-    ):
-        self._polylines.append(Polyline(points, point_name))
-
-        return self
-
-    @property
-    def polylines(self) -> list[Polyline]:
-        return self._polylines
-
-    @classmethod
-    def merge(
-        cls, name: str, bg: Tool, fg: Tool, position: tuple[int, int] = (0, 0)
-    ) -> Tool:
-        """Creates a Merge tool and automatically sets source inputs to connect other nodes."""
-
-        return (
-            Tool("Merge", name, position)
-            .add_source_input("Background", bg.name)
-            .add_source_input("Foreground", fg.name)
-        )
-
-    @classmethod
-    def background(
-        cls,
-        name: str,
-        color: RGBA = RGBA(),
-        resolution: tuple[int, int] | Literal["auto"] = "auto",
-        position: tuple[int, int] = (0, 0),
-    ) -> Tool:
-        """Creates a Background tool and automatically sets color and resolution inputs."""
-
-        bg = Tool("Background", name, position).add_inputs(
-            TopLeftRed=color.red,
-            TopLeftGreen=color.green,
-            TopLeftBlue=color.blue,
-            TopLeftAlpha=color.alpha,
-        )
-
-        if resolution == "auto":
-            bg.add_inputs(UseFrameFormatSettings=1)
-            return bg
-
-        return bg.add_inputs(
-            UseFrameFormatSettings=0, Width=resolution[0], Height=resolution[1]
-        )
-
+    # Alternate Constructors
     @classmethod
     def mask(
         cls,
@@ -137,8 +105,56 @@ class Tool:
     ) -> Tool:
         """Creates a Mask tool and automatically sets the correct output name for a mask."""
 
-        return Tool(f"{type}Mask", name, position, [Output(name, "Mask")])
+        return Tool(id=f"{type}Mask", name=name, position=position, output="Mask")
 
-    # Method aliases
-    bg = background
-    mrg = merge
+    @classmethod
+    def background(
+        cls,
+        name: str,
+        top_left: RGBA | None = None,
+        top_right: RGBA | None = None,
+        bottom_left: RGBA | None = None,
+        bottom_right: RGBA | None = None,
+        resolution: tuple[int, int] | Literal["auto"] = "auto",
+        position: tuple[int, int] = (0, 0),
+    ) -> Tool:
+        """Creates a Background tool and automatically sets color and resolution inputs."""
+
+        bg = (
+            Tool("Background", name, position)
+            .add_color_input(top_left, "TopLeft")
+            .add_color_input(top_right, "TopRight")
+            .add_color_input(bottom_left, "BottomLeft")
+            .add_color_input(bottom_right, "BottomRight")
+        )
+
+        if resolution == "auto":
+            return bg.add_inputs(UseFrameFormatSettings=1)
+
+        return bg.add_inputs(
+            UseFrameFormatSettings=0, Width=resolution[0], Height=resolution[1]
+        )
+
+    @classmethod
+    def merge(
+        cls,
+        name: str,
+        background: Tool | None,
+        foreground: Tool | None,
+        position: tuple[int, int],
+    ) -> Tool:
+        mrg = Tool("Merge", name, position)
+
+        if bg is not None:
+            bg = Input(
+                "Background", source_operator=background.name, source=background.output
+            )
+            mrg.add_input(bg)
+
+        if fg is not None:
+            fg = Input(
+                "Foreground", source_operator=foreground.name, source=foreground.output
+            )
+            mrg.add_input(fg)
+
+        return mrg
