74d6f0571cca98d51043c7eb0629f3652ff5903f
Bruno Reis <85531625+brunocbreis@users.noreply.github.com>
2023-02-15 18:00:46 -0300

    change build method to use pyproject

--- setup.py
+++ setup.py
@@ -1,13 +1,4 @@
-import setuptools
+from setuptools import setup
 
-setuptools.setup(
-    name="pysion",
-    version="0.1",
-    description="Package for creating code that's readable by DaVinci Resolve Fusion.",
-    url="https://github.com/brunocbreis/pysion",
-    author="Bruno Reis",
-    install_requires=[],
-    author_email="brunocbreis@gmail.com",
-    packages=setuptools.find_packages(),
-    zip_safe=False,
-)
+if __name__ == "__main__":
+    setup()
